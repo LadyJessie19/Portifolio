00b504d54b3f43786afd53eb3593c22c8888cd1d
Jessie <jessie.moura19@gmail.com>
2024-02-09 01:13:45 -0400

    fix: window reload attempt 7

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -24,11 +24,41 @@ function App() {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  // useEffect(() => {
-  //   if (window.location.pathname === "/") {
-  //     navigate("/us");
-  //   }
-  // });
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      navigate("/us");
+    }
+    if (window.location.pathname === "*") {
+      navigate("/us");
+    }
+  });
+
+  useEffect(() => {
+    if (window.location.pathname === "/us") {
+      navigate("/us");
+    }
+    if (window.location.pathname === "/us/about-me") {
+      navigate("/us/about-me");
+    }
+    if (window.location.pathname === "/us/contacts") {
+      navigate("/us/contacts");
+    }
+    if (window.location.pathname === "/us/projects") {
+      navigate("/us/projects");
+    }
+    if (window.location.pathname === "/br") {
+      navigate("/br");
+    }
+    if (window.location.pathname === "/br/sobre-mim") {
+      navigate("/br/sobre-mim");
+    }
+    if (window.location.pathname === "/br/contatos") {
+      navigate("/br/contatos");
+    }
+    if (window.location.pathname === "/br/projetos") {
+      navigate("/br/projetos");
+    }
+  }, [pathname]);
 
   return (
     <>
@@ -46,7 +76,7 @@ function App() {
             <Route path="/br/sobre-mim" element={<AboutMePt />} />
             <Route path="/br/contatos" element={<ContactsPt />} />
             <Route path="/br/projetos" element={<ProjectsPt />} />
-            <Route path="*" element={<HomePt />} />
+            <Route path="*" element={<HomeEn />} />
           </Routes>
         </ThemeProvider>
       </LanguageProvider>
